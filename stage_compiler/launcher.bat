22e633b5aa02afebd5a13679e4ef5b4b43c9605f
git <chellal-halim@hotmail.fr>
2016-11-06 08:06:19 +0100

    clean new branch

--- stage_compiler/launcher.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-flex lexer.l
-bison -dy source.y
-gcc lex.yy.c y.tab.c -o ..\stage_maker\StageMaker\spell_compiler.exe
-..\stage_maker\StageMaker\spell_compiler.exe out.txt < script.txt
-pause
